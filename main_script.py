121bd8bf97d156cfe84636f4829c1671ab376683
Nathan Wright <nathantwright@me.com>
2025-04-23 16:23:56 +0100

    make output_pokemon_data() more flexible

--- main_script.py
+++ main_script.py
@@ -15,19 +15,21 @@ def get_pokemon_data(pokemon_name:str) -> dict:
 user_pokemon_data = get_pokemon_data(user_pokemon)
 CPU_pokemon_data = get_pokemon_data(CPU_pokemon)
 
-# to get ability
-abilities = user_pokemon_data['abilities'][0]
-ability = abilities['ability']
-
-# to format height and weight properly
-height = int(user_pokemon_data['height'])
-weight = int(user_pokemon_data['weight'])
-height_formatted = height / 10
-weight_formatted = weight / 10
-
 # Print the pokemon's data
-def output_pokemon_data():
-    print('Name: {}'.format(user_pokemon_data['name']))
+def output_pokemon_data(pokemon_data):
+    # to get ability
+    abilities = user_pokemon_data['abilities'][0]
+    ability = abilities['ability']
+
+    # to format height and weight properly
+    height = int(pokemon_data['height'])
+    weight = int(pokemon_data['weight'])
+    height_formatted = height / 10
+    weight_formatted = weight / 10
+
+    print('Name: {}'.format(pokemon_data['name']))
     print('Weight: {}'.format(weight_formatted) + "(kgs)")
     print('Height: {}'.format(height_formatted) + "(m)")
     print('Ability: {}'.format(ability['name']))
+
+output_pokemon_data(user_pokemon_data)
