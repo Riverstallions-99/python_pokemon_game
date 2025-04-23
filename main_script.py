ddb270107607999c43fe1aea3452be1e81880ee2
hello-world-5 <hello-world-5@unknown.home>
2025-04-23 16:53:14 +0100

    Refactor user pokemon assignment into separate function

--- main_script.py
+++ main_script.py
@@ -1,21 +1,9 @@
 # Main code which pulls in battle_pokemon and the pick_pokemon modules
-from pick_pokemon import user_picks_pokemon, pick_random_pokemon
+from pick_pokemon import user_picks_pokemon, pick_random_pokemon, user_pokemon_assignment
 import requests, json, random
 
 CPU_pokemon = pick_random_pokemon()
-user_prompt = True
-while user_prompt:
-    assign_player_random_pokemon = input("Would you like to:\n1. Choose your own pokemon\n2. Get assigned a random pokemon\nEnter 1 or 2: ")
-    if assign_player_random_pokemon.isdigit() and (int(assign_player_random_pokemon) == 1 or int(assign_player_random_pokemon) == 2):
-        user_prompt = False
-        assign_player_random_pokemon = bool(int(assign_player_random_pokemon)-1)
-    else:
-        print("Invalid input.")
-
-if assign_player_random_pokemon:
-    user_pokemon = pick_random_pokemon()
-else:
-    user_pokemon = user_picks_pokemon()
+user_pokemon = user_pokemon_assignment()
 
 def get_pokemon_data(pokemon_name:str) -> dict:
     # Get the pokemon's data from the API
--- pick_pokemon.py
+++ pick_pokemon.py
@@ -2,6 +2,22 @@ import requests
 import json
 import random
 
+def user_pokemon_assignment() -> str:
+    user_prompt = True
+    while user_prompt:
+        assign_player_random_pokemon = input("Would you like to:\n1. Choose your own pokemon\n2. Get assigned a random pokemon\nEnter 1 or 2: ")
+        if assign_player_random_pokemon.isdigit() and (int(assign_player_random_pokemon) == 1 or int(assign_player_random_pokemon) == 2):
+            user_prompt = False
+            assign_player_random_pokemon = bool(int(assign_player_random_pokemon)-1)
+        else:
+            print("Invalid input.")
+
+    if assign_player_random_pokemon:
+        user_pokemon = pick_random_pokemon()
+    else:
+        user_pokemon = user_picks_pokemon()
+    return user_pokemon
+            
 def user_picks_pokemon() -> str:
     # Get the list of pokemon from the API
     url = 'https://pokeapi.co/api/v2/pokemon/'
