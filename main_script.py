5f53685039fd50536c32ce4cdb9a20b009eb1ac8
Rebecca <white.rebecca2012@gmail.com>
2025-04-24 09:40:10 +0100

    Remove redundant code and imports

--- main_script.py
+++ main_script.py
@@ -2,13 +2,9 @@
 from pick_pokemon import user_picks_pokemon, pick_random_pokemon, user_pokemon_assignment
 from battle_pokemon import battle
 from fetch_pokemon_data import get_pokemon_data, output_pokemon_data
-import requests, json, random
 
-CPU_pokemon = pick_random_pokemon()
-user_pokemon = user_pokemon_assignment()
-
-user_pokemon_data = get_pokemon_data(user_pokemon)
-CPU_pokemon_data = get_pokemon_data(CPU_pokemon)
+user_pokemon_data = get_pokemon_data(user_pokemon_assignment())
+CPU_pokemon_data = get_pokemon_data(pick_random_pokemon())
 
 print("\nYour pokemon:")
 output_pokemon_data(user_pokemon_data)
